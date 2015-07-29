41cc5773198f65e44ccf774bd32e1d925ae99fd7
Thi Nguyen <fanslations@gmail.com>
2015-07-29 10:23:24 -0400

    comment page fixed

---
+++ Paranovels.Tests/Paranovels.Facade/FeedFacade_Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Paranovels.Common;
+using Paranovels.DataModels;
+using Paranovels.Facade;
+
+namespace Paranovels.Tests.Paranovels.Facade
+{
+    [TestClass]
+    public class FeedFacade_Tests
+    {
+        [TestMethod]
+        public void CheckFeed_Test()
+        {
+
+            var feedFacade = new FeedFacade();
+
+            //var groupResult = feedFacade.CheckFeed(R.ConnectorType.GROUP_FEED);
+            var seriesResult = feedFacade.CheckFeed(R.ConnectorType.SERIES_FEED);
+        }
+    }
+}
